302d2336a8e83df518359b6dffee71cef9906d62
*Cng6ZrP6Jb <papoti@clarenet.co.jp>
2022-03-11 17:35:31 +0900

    Created a simple carousel with Vue3 + Component API

---
+++ 2-carousel/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import Home from './components/Home.vue'
+</script>
+
+<template>
+  <Home />
+</template>
+
+<script>
+export default {
+  components: {
+    Home
+  },
+  data () {
+    return {
+    }
+  },
+}
+</script>
+
+<style>
+*, *::after, *::before {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ 2-carousel/src/components/carousel/Slide.vue
@@ -0,0 +1,37 @@
+<script setup>
+</script>
+
+<script>
+export default {
+    data () {
+        return {
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="slide">
+        <transition name="slide">
+            <slot></slot>
+        </transition>
+    </div>
+</template>
+
+<style scoped>
+    .slide-enter-from, 
+    .slide-leave-to {
+        opacity: 0;
+        /* transform: translateX(0) */
+    }
+    .slide-leave-from, 
+    .slide-enter-to {
+        opacity: 1;
+        /* transform: translateX(-100%) */
+    }
+    .slide-enter-active,
+    .slide-leave-active {
+        /* transition: all 0.8s ease-out; */
+        transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+    }
+</style>
